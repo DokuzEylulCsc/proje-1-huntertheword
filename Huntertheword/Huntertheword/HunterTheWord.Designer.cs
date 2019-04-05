85f121b1e0170882f6993a8fabff5a437352719d
aysealtun <47896675+aysealtun@users.noreply.github.com>
2019-04-06 02:36:11 +0300

    Süre eklendi. Süreye ve girdiye göre tepki eklendi.

--- Huntertheword/Huntertheword/HunterTheWord.Designer.cs
+++ Huntertheword/Huntertheword/HunterTheWord.Designer.cs
@@ -28,21 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.kontrolEt = new System.Windows.Forms.Button();
             this.Skor = new System.Windows.Forms.Label();
             this.SkorSayı = new System.Windows.Forms.Label();
             this.kelimeGirisi = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.sureLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.oyunBitti = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // kontrolEt
             // 
             this.kontrolEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.kontrolEt.Location = new System.Drawing.Point(239, 442);
-            this.kontrolEt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.kontrolEt.Location = new System.Drawing.Point(179, 359);
+            this.kontrolEt.Margin = new System.Windows.Forms.Padding(2);
             this.kontrolEt.Name = "kontrolEt";
-            this.kontrolEt.Size = new System.Drawing.Size(251, 42);
+            this.kontrolEt.Size = new System.Drawing.Size(188, 34);
             this.kontrolEt.TabIndex = 0;
             this.kontrolEt.Text = "KONTROL ET";
             this.kontrolEt.UseVisualStyleBackColor = true;
@@ -53,9 +56,10 @@
             this.Skor.AutoSize = true;
             this.Skor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.Skor.ForeColor = System.Drawing.Color.Red;
-            this.Skor.Location = new System.Drawing.Point(13, 14);
+            this.Skor.Location = new System.Drawing.Point(10, 11);
+            this.Skor.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.Skor.Name = "Skor";
-            this.Skor.Size = new System.Drawing.Size(64, 25);
+            this.Skor.Size = new System.Drawing.Size(51, 20);
             this.Skor.TabIndex = 1;
             this.Skor.Text = "Skor:";
             // 
@@ -64,18 +68,19 @@
             this.SkorSayı.AutoSize = true;
             this.SkorSayı.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.SkorSayı.ForeColor = System.Drawing.Color.Red;
-            this.SkorSayı.Location = new System.Drawing.Point(83, 14);
+            this.SkorSayı.Location = new System.Drawing.Point(62, 11);
+            this.SkorSayı.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.SkorSayı.Name = "SkorSayı";
-            this.SkorSayı.Size = new System.Drawing.Size(24, 25);
+            this.SkorSayı.Size = new System.Drawing.Size(19, 20);
             this.SkorSayı.TabIndex = 2;
             this.SkorSayı.Text = "0";
             // 
             // kelimeGirisi
             // 
-            this.kelimeGirisi.Location = new System.Drawing.Point(239, 401);
-            this.kelimeGirisi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.kelimeGirisi.Location = new System.Drawing.Point(179, 326);
+            this.kelimeGirisi.Margin = new System.Windows.Forms.Padding(2);
             this.kelimeGirisi.Name = "kelimeGirisi";
-            this.kelimeGirisi.Size = new System.Drawing.Size(251, 22);
+            this.kelimeGirisi.Size = new System.Drawing.Size(189, 20);
             this.kelimeGirisi.TabIndex = 3;
             // 
             // label1
@@ -83,9 +88,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(576, 14);
+            this.label1.Location = new System.Drawing.Point(432, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 25);
+            this.label1.Size = new System.Drawing.Size(52, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Süre:";
             // 
@@ -94,26 +100,48 @@
             this.sureLabel.AutoSize = true;
             this.sureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.sureLabel.ForeColor = System.Drawing.Color.Red;
-            this.sureLabel.Location = new System.Drawing.Point(674, 14);
+            this.sureLabel.Location = new System.Drawing.Point(506, 11);
+            this.sureLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.sureLabel.Name = "sureLabel";
-            this.sureLabel.Size = new System.Drawing.Size(54, 25);
+            this.sureLabel.Size = new System.Drawing.Size(44, 20);
             this.sureLabel.TabIndex = 5;
             this.sureLabel.Text = "süre";
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // oyunBitti
+            // 
+            this.oyunBitti.AutoSize = true;
+            this.oyunBitti.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.oyunBitti.ForeColor = System.Drawing.Color.Red;
+            this.oyunBitti.Location = new System.Drawing.Point(170, 70);
+            this.oyunBitti.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.oyunBitti.MaximumSize = new System.Drawing.Size(300, 0);
+            this.oyunBitti.Name = "oyunBitti";
+            this.oyunBitti.Size = new System.Drawing.Size(77, 31);
+            this.oyunBitti.TabIndex = 6;
+            this.oyunBitti.Text = "asda";
+            this.oyunBitti.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.oyunBitti.Visible = false;
+            // 
             // HunterTheWord
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = global::Huntertheword.Properties.Resources.izmir_saat_kulesi_genel_gorunum;
-            this.ClientSize = new System.Drawing.Size(740, 508);
+            this.ClientSize = new System.Drawing.Size(555, 413);
+            this.Controls.Add(this.oyunBitti);
             this.Controls.Add(this.sureLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.kelimeGirisi);
             this.Controls.Add(this.SkorSayı);
             this.Controls.Add(this.Skor);
             this.Controls.Add(this.kontrolEt);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "HunterTheWord";
             this.Text = "Hunter The Word";
@@ -130,5 +158,7 @@
         private System.Windows.Forms.TextBox kelimeGirisi;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label sureLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label oyunBitti;
     }
 }
--- Huntertheword/Huntertheword/HunterTheWord.cs
+++ Huntertheword/Huntertheword/HunterTheWord.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Huntertheword
@@ -13,34 +6,65 @@ namespace Huntertheword
     public partial class HunterTheWord : Form
     {
         private bool girdiDogruMu;
+        Oyun oyun;
 
-        public HunterTheWord()
+        public HunterTheWord(Oyun oyun)
         {
+            this.oyun = oyun;
             InitializeComponent();
+            sureLabel.Text = oyun.OyunSuresi.ToString();
         }
 
 
         private void kontrolEt_Click(object sender, EventArgs e)
         {
-            /*
+
+
+            //oyun süresi bitmişse tekrar başlatamamsı için if kontrolü
+            if (oyun.OyunSuresi > 0)
+            {
+                /*
              * burdaki 3 şu anki tahminin olması gereken uzunluk bunu bir sayaç yardımıyla ilerletmemiz lazım
              * şimdilik 3 olarak yazdım.
              */
-            girdiDogruMu = Oyuncu.KullanicidanGirdiAl(kelimeGirisi.Text, 3);
-            Console.WriteLine("girdi: " + girdiDogruMu);
-            TepkiVer();
+
+                //girdi kontrolü yapıp kullanıcıya tepki veriyor.
+                girdiDogruMu = Oyuncu.KullanicidanGirdiAl(kelimeGirisi.Text, 3);
+                if (!girdiDogruMu)
+                {
+                    TepkiVer("Tahmininiz olmaması gereken bir karakter içeriyor!");
+                }
+                else
+                {
+                    oyunBitti.Visible = false;
+                }
+
+                //oyun süresini ilk tahmin gönderilene kadar başlatmıyoruz.
+                timer1.Start();
+
+            }
         }
 
-        private void TepkiVer()
+        private void TepkiVer(string tepki)
         {
-            /*
-             * girdi kontrolü yapıldıktan sonra kullanıcıya görsel olarak tepki verilmesi gerekiyor. 
-             * Bu metodu temp olarak açtım değiştirilebilir.
-             */
+            //kullanıcıya bu metod sayesinde tepki veriyoruz.
+            oyunBitti.Visible = true;
+            oyunBitti.Text = tepki;
         }
-        public void sureGonder(int sure)
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            sureLabel.Text = " " + sure;
+            //her saniye oyun süresinden azalıyor.oyunun bitip bitmediğini kontrol edip kullanıcıya tepki veriyor.
+            oyun.OyunSuresi--;
+            sureLabel.Text = oyun.OyunSuresi.ToString();
+            timer1.Start();
+
+            if (oyun.OyunSuresi == 0)
+            {
+                timer1.Stop();
+                TepkiVer("Süreniz Doldu!");
+                oyun.sureBitti();
+            }
         }
     }
 }
