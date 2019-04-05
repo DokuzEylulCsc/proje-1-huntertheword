fe7059a9efb3c10f3c967ca41e7d6abd37eebef7
aysealtun <47896675+aysealtun@users.noreply.github.com>
2019-04-06 02:35:20 +0300

    Oyun seviye seçimleri yapıldı

--- Huntertheword/Huntertheword/Form1.Designer.cs
+++ Huntertheword/Huntertheword/Form1.Designer.cs
@@ -29,75 +29,22 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.Baslat = new System.Windows.Forms.Button();
-            this.KolayButton = new System.Windows.Forms.RadioButton();
-            this.OrtaButton = new System.Windows.Forms.RadioButton();
-            this.ZorButton = new System.Windows.Forms.RadioButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.bilgiButonu = new System.Windows.Forms.Button();
             this.derecelerButton = new System.Windows.Forms.Button();
+            this.kolayBtn = new System.Windows.Forms.Button();
+            this.ortaBtn = new System.Windows.Forms.Button();
+            this.zorBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // Baslat
-            // 
-            this.Baslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Baslat.Location = new System.Drawing.Point(138, 455);
-            this.Baslat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Baslat.Name = "Baslat";
-            this.Baslat.Size = new System.Drawing.Size(361, 50);
-            this.Baslat.TabIndex = 1;
-            this.Baslat.Text = "YENİ OYUN";
-            this.Baslat.UseVisualStyleBackColor = true;
-            this.Baslat.Click += new System.EventHandler(this.Baslat_Click);
-            // 
-            // KolayButton
-            // 
-            this.KolayButton.AutoSize = true;
-            this.KolayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.KolayButton.Location = new System.Drawing.Point(138, 407);
-            this.KolayButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.KolayButton.Name = "KolayButton";
-            this.KolayButton.Size = new System.Drawing.Size(80, 21);
-            this.KolayButton.TabIndex = 3;
-            this.KolayButton.TabStop = true;
-            this.KolayButton.Text = "KOLAY";
-            this.KolayButton.UseVisualStyleBackColor = true;
-            this.KolayButton.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
-            // 
-            // OrtaButton
-            // 
-            this.OrtaButton.AutoSize = true;
-            this.OrtaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.OrtaButton.Location = new System.Drawing.Point(276, 407);
-            this.OrtaButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.OrtaButton.Name = "OrtaButton";
-            this.OrtaButton.Size = new System.Drawing.Size(72, 21);
-            this.OrtaButton.TabIndex = 4;
-            this.OrtaButton.TabStop = true;
-            this.OrtaButton.Text = "ORTA";
-            this.OrtaButton.UseVisualStyleBackColor = true;
-            // 
-            // ZorButton
-            // 
-            this.ZorButton.AutoSize = true;
-            this.ZorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.ZorButton.Location = new System.Drawing.Point(437, 407);
-            this.ZorButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.ZorButton.Name = "ZorButton";
-            this.ZorButton.Size = new System.Drawing.Size(62, 21);
-            this.ZorButton.TabIndex = 5;
-            this.ZorButton.TabStop = true;
-            this.ZorButton.Text = "ZOR";
-            this.ZorButton.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(640, 359);
+            this.pictureBox1.Size = new System.Drawing.Size(480, 292);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
@@ -105,10 +52,10 @@
             // 
             this.bilgiButonu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.bilgiButonu.ForeColor = System.Drawing.Color.Coral;
-            this.bilgiButonu.Location = new System.Drawing.Point(12, 425);
-            this.bilgiButonu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bilgiButonu.Location = new System.Drawing.Point(9, 345);
+            this.bilgiButonu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.bilgiButonu.Name = "bilgiButonu";
-            this.bilgiButonu.Size = new System.Drawing.Size(87, 80);
+            this.bilgiButonu.Size = new System.Drawing.Size(82, 79);
             this.bilgiButonu.TabIndex = 7;
             this.bilgiButonu.Text = "Bilgi";
             this.bilgiButonu.UseVisualStyleBackColor = true;
@@ -118,45 +65,80 @@
             // 
             this.derecelerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.derecelerButton.ForeColor = System.Drawing.Color.Coral;
-            this.derecelerButton.Location = new System.Drawing.Point(523, 425);
+            this.derecelerButton.Location = new System.Drawing.Point(392, 345);
+            this.derecelerButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.derecelerButton.Name = "derecelerButton";
-            this.derecelerButton.Size = new System.Drawing.Size(105, 80);
+            this.derecelerButton.Size = new System.Drawing.Size(87, 79);
             this.derecelerButton.TabIndex = 8;
             this.derecelerButton.Text = "Dereceler";
             this.derecelerButton.UseVisualStyleBackColor = true;
             // 
+            // kolayBtn
+            // 
+            this.kolayBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kolayBtn.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.kolayBtn.Location = new System.Drawing.Point(105, 345);
+            this.kolayBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.kolayBtn.Name = "kolayBtn";
+            this.kolayBtn.Size = new System.Drawing.Size(65, 65);
+            this.kolayBtn.TabIndex = 9;
+            this.kolayBtn.Text = "Kolay";
+            this.kolayBtn.UseVisualStyleBackColor = true;
+            this.kolayBtn.Click += new System.EventHandler(this.kolayBtn_Click);
+            // 
+            // ortaBtn
+            // 
+            this.ortaBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ortaBtn.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.ortaBtn.Location = new System.Drawing.Point(202, 345);
+            this.ortaBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.ortaBtn.Name = "ortaBtn";
+            this.ortaBtn.Size = new System.Drawing.Size(65, 65);
+            this.ortaBtn.TabIndex = 10;
+            this.ortaBtn.Text = "Orta";
+            this.ortaBtn.UseVisualStyleBackColor = true;
+            this.ortaBtn.Click += new System.EventHandler(this.ortaBtn_Click);
+            // 
+            // zorBtn
+            // 
+            this.zorBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.zorBtn.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.zorBtn.Location = new System.Drawing.Point(299, 345);
+            this.zorBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.zorBtn.Name = "zorBtn";
+            this.zorBtn.Size = new System.Drawing.Size(65, 65);
+            this.zorBtn.TabIndex = 11;
+            this.zorBtn.Text = "Zor";
+            this.zorBtn.UseVisualStyleBackColor = true;
+            this.zorBtn.Click += new System.EventHandler(this.zorBtn_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(653, 554);
+            this.ClientSize = new System.Drawing.Size(490, 450);
+            this.Controls.Add(this.zorBtn);
+            this.Controls.Add(this.ortaBtn);
+            this.Controls.Add(this.kolayBtn);
             this.Controls.Add(this.derecelerButton);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.bilgiButonu);
-            this.Controls.Add(this.ZorButton);
-            this.Controls.Add(this.OrtaButton);
-            this.Controls.Add(this.KolayButton);
-            this.Controls.Add(this.Baslat);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Hunter The Word";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Baslat;
-        private System.Windows.Forms.RadioButton KolayButton;
-        private System.Windows.Forms.RadioButton OrtaButton;
-        private System.Windows.Forms.RadioButton ZorButton;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button bilgiButonu;
         private System.Windows.Forms.Button derecelerButton;
+        private System.Windows.Forms.Button kolayBtn;
+        private System.Windows.Forms.Button ortaBtn;
+        private System.Windows.Forms.Button zorBtn;
     }
 }
 
